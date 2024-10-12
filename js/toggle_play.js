5072fd21f3a8284a2140361058e025424718bbfe
PonderSlime <micah.j.edwards1@gmail.com>
2024-10-12 17:33:56 -0600

    Audio playing method changed

--- js/toggle_play.js
+++ js/toggle_play.js
@@ -1,37 +1,56 @@
 let mute = true;
-let 
 function togglePlay(audioT, isMuteButton) {
     var bg = document.getElementById("bg-music");
-    //var audioT = document.getElementById("bg-music");
+    x = document.getElementById("audioIcon");
+
     if (isMuteButton == 1) {
         mute = !mute;
     }
+/* 
     else if (isMuteButton == 0) {
         bg.currentTime = 0;
         bg.paused ? bg.play() : bg.pause();
+        bg.muted = true;
+    } */
+
+    if (mute==false) {
+        unmutePage()
+        x.classList.add('fa-volume-high');
+        x.classList.remove('fa-volume-xmark');
     }
-    x = document.getElementById("audioIcon");
-    if (audioT.paused&&mute==false) {
+    else if (mute==true) {
+        mutePage();
+        x.classList.remove('fa-volume-high');
+        x.classList.add('fa-volume-xmark');
+    }
+
+    if (audioT.paused && mute==false) {
+        mutePage();
+        audioT.muted = false;
         audioT.play();
-        if (mute==false) {
-            x.classList.add('fa-volume-high');
-            x.classList.remove('fa-volume-xmark');
-        }
     }
-    else if (!audioT.paused||mute==true){
+    else if (!audioT.paused) {
         audioT.pause();
         audioT.currentTime = 0;
-        if (mute==true) {
-            x.classList.remove('fa-volume-high');
-            x.classList.add('fa-volume-xmark');
-            mutePage();
-        }
     }
+
+
 };
+
 function mutePage() {
     document.querySelectorAll("audio").forEach((elem) => muteMe(elem));
 }
+
+function unmutePage() {
+    document.querySelectorAll("audio").forEach((elem) => unmuteMe(elem));
+}
+
 function muteMe(elem) {
-    elem.pause();
+    elem.muted=true;
+    elem.currentTime = 0;
+}
+
+function unmuteMe(elem) {
+    elem.muted=false;
     elem.currentTime = 0;
 }
